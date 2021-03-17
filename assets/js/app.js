cc1bac07e5dd68e3bef4cf93ec5186db96ac1fe8
siewla <siewlapang@gmail.com>
2021-03-17 20:20:31 +0800

    all projects

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('#project1').load('project1.html');
+  $('#project2').load('project2.html');
+  $('#project3').load('project3.html');
+  $('#project4').load('project4.html');
+});
